d87114a60f6b0af955522f9f48e4b010a5a17c23
komugico <shu.isaka.pc+develop@gmail.com>
2021-06-11 10:29:20 +0900

    #6 トップページの雛形作成

--- client/bgc/src/index.js
+++ client/bgc/src/index.js
@@ -1,10 +1,20 @@
+/* ========================================================================== */
+/* React-Redux                                                                */
+/* ========================================================================== */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import { BrowserRouter } from "react-router-dom";
+
+/* ========================================================================== */
+/* App Routers                                                                */
+/* ========================================================================== */
+import HomeRouter from './apps/home/HomeRouter';
 
 ReactDOM.render(
     <React.StrictMode>
-        <h1>Hello, React-Redux</h1>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <HomeRouter />
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
